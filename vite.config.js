86967ebefea711422cd7d3f98468e7ff872c8bc1
iwan hartanto <elop.development@gmail.com>
2024-04-06 09:31:40 +0700

    Migrate from poi to vite & update dependencies
    migrate builder from poi to vite

--- vite.config.js
+++ vite.config.js
@@ -21,9 +21,28 @@ const config = defineConfig({
 		},
 		dedupe: ['vue-demi'],
 	},
-
 	build: {
 		minify: true,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: path.resolve(__dirname, 'src/index.js'),
+      format: [ 'cjs', 'es', 'umd', 'umd-min' ],
+      name: 'VueGoodTable',
+      // the proper extensions will be added
+      fileName: 'vue-good-table',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
 	},
 
 	plugins: [
